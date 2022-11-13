31e1e4135fcbf5c76bbd9173f5bc98f339676b57
Noah-Waisbrod <20nsw@queensu.ca>
2022-11-13 14:44:22 -0500

    more formatting

--- WebCam.py
+++ WebCam.py
@@ -8,28 +8,34 @@ import numpy as np
 #greyscale images func
 
 
-#
+#ScreenGrab an image from video
 
+
+#Create folder and add images to it
+
+
+#video Feed
+def openVideo():
+    #open webcam and show in folder
+    vid = cv2.VideoCapture(0)
+  
+    while(True):
+      
+        # Capture the video frame
+        ret, frame = vid.read()
+  
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+      
+        # to exit program 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+  
+    # After the loop release the cap object
+    vid.release()
+
+    #Destroy all the windows
+    cv2.destroyAllWindows()
 
 #--------------------(Main)-------------------------#
-
-#open webcam and show in folder
-vid = cv2.VideoCapture(0)
-  
-while(True):
-      
-    # Capture the video frame
-    ret, frame = vid.read()
-  
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-      
-    # to exit program 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-  
-# After the loop release the cap object
-vid.release()
-
-# Destroy all the windows
-cv2.destroyAllWindows()
+openVideo()
