2e5a65560585471b4d9533dff97348ec10c6e710
SammiW15 <wxinransammi@gmail.com>
2022-11-20 00:02:51 -0500

    p

--- WebCam.py
+++ WebCam.py
@@ -12,26 +12,45 @@ def greyScale(img):
 #ScreenGrab an image from video
 
 
-#Create folder and add images to it
-def saveImg(name : np.ndarray):
-    
-
-#video Feed
+#video Feed & save webcam video as images
 def openVideo():
     #open webcam and show in folder
     vid = cv2.VideoCapture(0)
-  
-    while(True):
-      
+    #set webcam to the width and height the input camera supports
+    vid.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+    dir = "qmind-SLR"
+    name = "asl_webcam"
+    #create a folder to save the video frames
+    os.mkdir(dir)
+    base_path = os.path.join(dir, name)
+    
+    time = 0
+    while vid.isOpened():
+
         # Capture the video frame
         ret, frame = vid.read()
-  
+
+        if ret == False:
+            print("Can't receive frame (stream end?. Exiting ...")
+            break
+        
+        #flip the vide frame
+        frame = cv2.flip(frame, 0)
+
+        #save the video frame as images
+        if time % 50 == 0:
+            cv2.imwrite('{}_{}.{}'.format(base_path, str(time), 'jpg'), frame)
+
         # Display the resulting frame
         cv2.imshow('frame', frame)
-      
+
         # to exit program 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+        time +=1
   
     # After the loop release the cap object
     vid.release()
