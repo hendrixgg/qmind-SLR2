fixed pixel value scaling in rescale_image

process_image.py
@@ -1,6 +1,4 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import ndimage
 import cv2
 
@@ -15,8 +13,9 @@ def blur_image(img, blur_amount):
     return ndimage.gaussian_filter(img, sigma=blur_amount)
 
 # Rescale image to grayscale numpy array (28, 28, 1) with values 0 to 1
+# ASSUMING THE PIXEL VALUES ARE FROM 0-255 IN THE FIRST PLACE
 def rescale_image(img):
-    return np.asarray(cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), dsize=(28, 28), interpolation=cv2.INTER_CUBIC))
+    return np.asarray(cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), dsize=(28, 28), interpolation=cv2.INTER_CUBIC)) / 255.
 
 def rescale_image_from_file(img_path):
     return rescale_image(cv2.imread(img_path))
@@ -24,6 +23,8 @@ def rescale_image_from_file(img_path):
 
 #--------------------(Main)-------------------------#
 def main():
+    import pandas as pd
+    import matplotlib.pyplot as plt
     # get image data from files #
     train_set, test_set = pd.read_csv('mnist_handsigns/sign_mnist_train.csv'), pd.read_csv('mnist_handsigns/sign_mnist_test.csv')
 
@@ -32,6 +33,9 @@ def main():
 
     img = train_images[0]
     raw_img = cv2.imread("LetterData/c/c0.png")
+    print(raw_img)
+    print(raw_img.shape)
+    
     # Showing some modified images #
     fig = plt.figure(figsize=(10, 5))
     ax1, ax2, ax3, ax4, ax5 = fig.subplots(1, 5)
@@ -44,6 +48,7 @@ def main():
     ax3.set_axis_off()
     ax4.imshow(cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY), cmap='gray')
     ax4.set_axis_off()
+    print(rescale_image(raw_img))
     print(rescale_image(raw_img).shape)
     ax5.imshow(rescale_image(raw_img), cmap='gray')
     ax5.set_axis_off()

test_model.py
@@ -22,7 +22,8 @@ def predict(img):
         return "ERROR"
     return get_label(np.argmax(model.predict(np.asarray([img]), verbose=0)))
 
-# takes an unformatted cv2 image and predicts the ASL handsign in it 
+# takes an unformatted cv2 image and predicts the ASL handsign in it
+# ASSUMES THE IMAGE HAS PIXEL VALUES FROM 0-255
 def predict_unformatted(img):
     return predict(rescale_image(img))
 