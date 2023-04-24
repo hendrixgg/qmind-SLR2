5f3b509b3fbb41540635a3939cef1e859dc4c603
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-04-24 13:40:09 -0400

    postprocess works

--- onnx_implementation/postprocess.py
+++ onnx_implementation/postprocess.py
@@ -2,11 +2,16 @@
 import numpy
 # label_map = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '', 'del', ' ']
 
+# returns numpy array of probabilities for each output
 def postprocess(out, labels, outputNames):
     # 0-25 for letters A-Z, 26-27 for empty character ('') and del
-    output_probabilities = numpy.zeros(shape=28)
-    for letter_idx, prob in out[outputNames[1]][0]:
-        output_probabilities[letter_idx] = prob
+    # output_probabilities = numpy.zeros(shape=(len(out[outputNames[1]]), 28))
+    output_probabilities = numpy.zeros(shape=(len(out[1]), 28))
+
+    # for output_idx, output in enumerate(out[outputNames[1]]):
+    for output_idx, output in enumerate(out[1]):
+        for letter_idx, prob in output.items():
+            output_probabilities[output_idx][letter_idx] = prob
 
     return output_probabilities
 
--- onnx_implementation/preprocess.py
+++ onnx_implementation/preprocess.py
@@ -3,13 +3,18 @@ from itertools import chain
 import numpy as np
 
 # input should be the hand landmarks for one hand as outputted from mediapipe.solutions.hands.process(frame), where frame is a 3 channel image in the RGB format
+# for now the input will be just a list of the hand datapoints for each frame along with a timestamp
 def preprocess(bytes, inputNames):
 
     # DECLARE FEEDS DICTIONARY
     feeds = dict()
-    inputNames = str(inputNames[0])
+    inputName = str(inputNames[0])
 
     # Transform the input as necessary
     # in this case that means converting the hand landmarks into a numpy array of shape (, 60)
-    bytesInput = np.frombuffer(bytes)
-    npBytes = 
+    bytesVal = np.frombuffer(bytes)
+    feedVal = np.bytesVal.astype(np.float32)
+
+    feeds[inputName] = feedVal
+
+    return feeds
